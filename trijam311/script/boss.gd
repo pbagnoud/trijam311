19077a89525987d7e2a4517deb59f9457edd7e9c
VB <119983496+vibonnier@users.noreply.github.com>
2025-03-02 17:02:26 +0100

    area boss

--- trijam311/script/boss.gd
+++ trijam311/script/boss.gd
@@ -1,18 +1,1 @@
 extends Node2D
-
-@export var joueur: Node2D
-
-@export var Animated_boss: AnimatedSprite2D
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	
-	# Calculer la distance entre l'AnimatedSprite et le joueur
-	var distance = global_position.distance_to(joueur.global_position)
-	
-	# Changer l'animation en fonction de la distance
-	if distance < 200:
-		Animated_boss.boss_carapace
-	else:
-		Animated_boss.boss_standard
-
