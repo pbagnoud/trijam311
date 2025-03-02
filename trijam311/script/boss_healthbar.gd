f7d5300607c9d8b87293bb23fa39d8928ca730ea
SolennEstier <solenn.estier@gmail.com>
2025-03-02 16:24:43 +0100

    Started health bar

---
+++ trijam311/script/boss_healthbar.gd
@@ -0,0 +1,17 @@
+extends ProgressBar
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func little_damage():
+	value += -1
+	
+func big_damage():
+	value += -10
